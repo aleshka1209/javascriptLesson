e0620186b3f1ba6619d081a0a12d3f0b25ac3cc0
Alisher <develepor@mail.ru>
2021-12-19 14:32:07 +0600

    js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,86 @@
-console.log('Alisher');
-console.log('git');
+"use strict";
+
+
+
+const personalMovieDB = {
+    count: 0,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: true,
+    start: function(){
+        personalMovieDB.count = +prompt('Сколько фильмов вы просмотрели', 1);
+
+        while(personalMovieDB.count == '' || personalMovieDB.coun == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt('Сколько фильмов вы просмотрели', 1);
+        }
+    },
+    rememberMyFilms: function(){
+        for(let i = 0; i < 2; i++){
+            let a = prompt('Один из просмотренных фильмов', 'Логан'),
+                b = prompt('На сколько его оцените', 5);
+        
+        if(a != null && b != null && a != '' && b != '' && a.length < 50){
+                personalMovieDB.movies[a] = b;
+                console.log('done');
+            }else{
+                console.log('error');
+                i--;
+            }
+        }    
+    },
+    detectPersonalLevel: function (){
+        if(personalMovieDB.count < 10){
+            console.log('Просмотрено довольна мало фильмов');
+        }else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+            console.log('Вы классический зритель');
+        }else if(personalMovieDB.count >=30){
+            console.log('Вы киноман');
+        }else{
+            console.log('Произошла ошибка');
+        }
+    },
+    showMyDB: function (obj){
+        if(!obj){
+            console.log(personalMovieDB);
+        }
+    },
+    toggleVisibleMyDB: function(){
+        if(personalMovieDB.privat){
+            personalMovieDB.privat = false;
+        } else{
+            personalMovieDB.privat = true;
+        }
+    },
+    writeYourGenres: function() {
+        for(let i = 1; i < 2; i++){
+            // let genre = prompt(`Ваш любимый жанр под номером ${i}`);
+            // if(genre === '' || genre === null){
+            //     console.log('Вы ввели некорректные данные или не ввели их вовсе');
+            //     i--;
+            // }else{
+            //     personalMovieDB.genres[i - 1] = genre; 
+            // }
+
+            let genres = prompt(`Введите ваши любимые ваши жанры чере запятую`).toLowerCase();
+            if(genres === '' || genres === null){
+                console.log('Вы ввели некорректные данные или не ввели их вовсе');
+                i--;
+            }else{
+                personalMovieDB.genres = genres.split(', '); 
+                personalMovieDB.genres.sort();
+            }
+
+
+        }
+        personalMovieDB.genres.forEach((item, i) => {
+            console.log(`Любимы жанр ${i + 1} - это ${item}`);
+        }); 
+    }
+};
+
+
+
+
+
+
--- js/script.js
+++ js/script.js
@@ -1,15 +1,66 @@
 "use strict";
-// console.log("arr" + " - object");
-// console.log("4" +  + "5");
 
-// let incr = 10,
-//     decr = 10;
+// Методы для поиска массива: Array.includes, Array.indexOf, Array.lastindexOf, Array.find, Array.findindex
 
-// console.log(5%2);
+// 1) Array.includes
 
-console.log(2 + 2 * 2 !== '6');
+const numbers = [38, 293, 139, 45, 10, 139];
 
-const isChecked = false,
-      isClose = false;
-console.log(isChecked || !isClose );
+// console.log(numbers.includes(139, 3));
+// console.log(numbers.includes(100));
 
+// 2) Array.indexOf
+
+// console.log(numbers.indexOf(38));
+// console.log(numbers.indexOf(100));
+
+// 3) Array.lastindexOf
+
+// console.log(numbers.lastIndexOf(139));
+// console.log(numbers.lastIndexOf(100));
+
+// const users = [
+//     {
+//         id: 1,
+//         username: "Michael",
+//         email: "michael@gmail.com"
+//     },
+//     {
+//         id: 2,
+//         username: "Joyce",
+//         email: "joyce@gmail.com"
+//     },
+//     {
+//         id: 3,
+//         username: "Jeremiah",
+//         email: "jeremiah@gmail.com"
+//     },
+//     {
+//         id: 4,
+//         username: "Urtnasan",
+//         email: "urta@gmail.com"
+//     }
+// ];
+
+// const finderUser = userId => users.findIndex(user => user.id == userId);
+
+// console.log(finderUser(4));
+
+// let x = 5; alert(++x);
+// console.log([] + false - null + true);
+
+// console.log(2 && 1 && null && 0 && undefined);
+// и запинается на лжи
+//  или запинается на правде
+
+// console.log(!!( 1 && 2 ) ===  (1 && 2));
+// console.log( null || 2 && 3 || 4 );
+
+// const a = [1, 2 , 3];
+// const b = [1, 2 , 3];
+// console.log(a == b); 
+
+// console.log(+"infinity");
+
+// console.log("Ёжик" > "яблоко");
+console.log(0 || "" || 2 || undefined || true || falsе );
